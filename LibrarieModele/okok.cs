cbf7c0f02dac07a5eb7f548b5a5ac349238ca0c1
irina-cosovan <irina.cosovan@student.usv.ro>
2022-04-03 01:04:43 +0300

    add class okok

---
+++ LibrarieModele/okok.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarieClase
+{
+    class okok
+    {
+        const bool VALIDAT = true;
+
+        string nume;
+        string prenume;
+
+        public okok(string nume, string prenume)
+        {
+            this.nume = nume;
+            this.prenume = prenume;
+        }
+    }
+}
